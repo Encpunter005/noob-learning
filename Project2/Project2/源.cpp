4ff4fcf355960b0cce5e9efb4be72b4785e1c3a7
Encounter005 <110457617+Encpunter005@users.noreply.github.com>
2022-08-26 11:56:23 +0800

    Add files via upload

--- Project2/Project2/源.cpp
+++ Project2/Project2/源.cpp
@@ -66,44 +66,76 @@
 //}
 
 //static - 静态的
-//1.static 修饰局部变量
-//2.修饰全局变量
-//3.修饰函数 
+//1.static 修饰局部变量，改变了局部变量的生命周期（本质上改变了变量的存储类型）
+//2.修饰全局变量,使得这个全局变量只能在自己所在的源文件（.c）内部可以使用，其他源文件不能使用！
+// 全局变量，在其他源文件内部可以被使用，是因为全局变量具有外部链接属性，但是被static修饰之后，就变成了内部属性链接，其他源文件就不能链接到这个静态的全局变量
+//3.修饰函数 ,使得函数只能在自己所在的源文件内部使用，不能在其他源文件内部使用
+// 本质上：static是将函数的外部链接属性变成了内部链接属性！（和static修饰全局变量一样）
+// 内存分为栈区、堆区、静态区
+// 栈区 局部变量函数的参数
+//堆区 动态内存分配
+//静态区 全局变量 static修饰的静态变量
 
 
-void test()
-{
-	int a = 1;
-	a++;
-	printf("%d\n", a);
+//void test()
+//{
+//	int a = 1;
+//	a++;
+//	printf("%d\n", a);
+//
+//
+//}//a已被销毁
 
-
-}
-
-
-int main()
-{
-	int i = 0;
-	while (i < 10)
-	{
-		test();
-		i++;
-	
-	}
-
-
-	return 0;
-}
 
 //int main()
 //{
-//	int a = 1;
-//	int b = 2;
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
 //	
-//	printf("请输入两个整数\n");
-//	scanf("%d %d", &a, &b);
-//	int c = a + b;
-//	printf("%d\n", c);
-//	return 0; 
+//	}
+//
+//
+//	return 0;
 //}
 
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d\n", a);
+//
+//}//a没有销毁
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//
+//	return 0;
+//}
+//extern声明外部符号的
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n",g_val);
+//	return 0;
+//
+//}
+//声明函数
+extern int add(int x, int y);
+int main()
+{
+
+	int a = 10;
+	int b = 20;
+	int sum = add(a, b);
+	printf("sum = %d\n", sum);
+	return 0;
+}
