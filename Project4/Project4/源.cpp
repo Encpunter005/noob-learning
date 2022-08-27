a11690bd54c4a0f9c381ca5572c83546cf0c2655
Encounter005 <110457617+Encpunter005@users.noreply.github.com>
2022-08-28 00:01:51 +0800

    Add files via upload

---
+++ Project4/Project4/源.cpp
@@ -0,0 +1,107 @@
+#define  _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+//分支语句
+//if switch
+//循环语句
+// while for (do while)
+//
+//多分支语句
+//int main()
+//{
+//	int a = 0;
+//	printf("你的未来是怎样？(1/2/3)\n");
+//	scanf("%d", &a);
+//	if (a == 1)
+//	{
+//		printf("成功拿到好offer\n");
+//	}
+//	else if (a == 2)
+//	{
+//		printf("创业失败，继承亿万家产\n");
+//	}
+//	else
+//	{
+//		printf("回家卖红薯\n");//只管第一条语句(不带括号)
+//	}
+//
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 60;
+//	if (age < 18);
+//	{
+//		printf("青年");
+//	}
+//	else if (age >= 18 && age < 26)//&&并且
+//	{
+//		printf("青年");
+//	}
+//
+//	else if (age >= 40 && age < 70)
+//	{
+//		printf("壮年");
+//	}
+//		
+//	else if (age >= 60 && age <= 400);
+//	{
+//		printf("老年");
+//	}
+//	
+//	return 0;
+//}
+//c语言中0表示假，非0表示真
+//悬空else
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//	{
+//
+//		if (b == 2)
+//			printf("hehe\n");
+//		else
+//			printf("haha\n");
+//	}
+//		return 0;
+//}//else跟它离的最近的if匹配
+//《高质量c++》
+//"老司机写法"
+//int main()
+//{
+//	int num = 3;
+//	if (5 == num)
+//		printf("hehe\n");
+//	return 0;
+//
+//
+//}
+//test
+//1.判断一个数是否为奇数
+//2.输出1-100之间的奇数
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (1 == a % 2)
+//	{
+//		printf("是奇数\n");
+//	}
+//	int i = 0;
+//	while ( i < 100)
+//	{
+//	
+//		i++;
+//		if (1 == i % 2)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	
+//	
+//	return 0;
+//}
