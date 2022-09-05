24a9ffad3a98526d62fefa61ac12500b5d42bd9a
Encounter005 <110457617+Encpunter005@users.noreply.github.com>
2022-09-05 21:54:45 +0800

    Add files via upload

--- Project4/Project4/源.cpp
+++ Project4/Project4/源.cpp
@@ -671,45 +671,110 @@
 //	return 0;
 //}
 //输入数字，从大到小输出
-int main()
-{
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	//输入
-	scanf("%d %d %d", &a, &b, &c);
-	//调整
-	if (a > b)
-	{
-		int tmp = a;
-		a = b;
-		b = tmp;
-	}
-	if (a < c)
-	{
-		int tmp = a;
-		a = c;
-		c = tmp;
-
-	}
-	if (b < c)
-	{
-		int tmp = b;
-		b = c;
-		c = tmp;
-	
-	}
-	//输出
-	printf("%d %d %d\n", a, b, c);
-
-
-
-
-
-
-	return 0;
-}
-
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	//输入
+//	scanf("%d %d %d", &a, &b, &c);
+//	//调整
+//	if (a > b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	
+//	}
+//	//输出
+//	printf("%d %d %d\n", a, b, c);
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+//打印1-100之间所有3的倍数
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i<=100 ; i++)
+//	{
+//		if (0 == i % 3)
+//			printf("%d\n", i);
+//	
+//	
+//	}
+//
+//
+//
+//
+//	return 0;
+//}
+//给两个数求最大公约数
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d %d", &m, &n);
+//	//假设最大公约数，m、n的较小值
+//	int max = 0;
+//	if (m > n)
+//		max = n;
+//	else
+//		max = m;
+//	while (1)
+//	{
+//		if (m % max == 0 && n % max == 0)
+//		{
+//			printf("最大的值是:%d\n", max);
+//			break;
+//		}
+//		max--;
+//	}
+//
+//
+//	return 0;
+//}
+//辗转相除法
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int  mid = 0;
+//	while (m % n)// mid = m % n
+//	{
+//	
+//		mid = m % n;
+//		m = n;
+//		n = mid;
+//	
+//	
+//	}
+//	printf("%d\n", mid);
+//
+//
+//
+//
+//	return 0;
+//
+//}
 
 		
 
