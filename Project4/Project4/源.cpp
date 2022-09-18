61b6b87a57c1141fdf187557fe01cb6c619c906b
Encounter005 <110457617+Encpunter005@users.noreply.github.com>
2022-09-18 10:09:51 +0800

    Add files via upload

--- Project4/Project4/源.cpp
+++ Project4/Project4/源.cpp
@@ -368,32 +368,32 @@
 //
 //	return 0;
 //}
-//编写代码，眼hi多个字符从两端移动，向中间汇聚
+//编写代码，多个字符从两端移动，向中间汇聚
 #include <string.h>//strlen strcmp
 #include <Windows.h>
 #include <WinBase.h>
-//int main()
-//{
-//	char arr1[] = "welcome to bit!!!!!!!";
-//	char arr2[] = "#####################";
-//	int left = 0;
-//	int right = strlen(arr1) - 1;//数组下标从0开始
-//	while (left <= right)
-//	{
-//
-// 
-//
-//		arr2[left] = arr1[left];
-//		arr2[right] = arr1[right];
-//		printf("%s\n", arr2);
-//		//Sleep(600);
-//		//system("cls");//清空屏幕
-//		left++;
-//		right--;
-//	}
-//
-//	return 0;
-//}
+int main()
+{
+	char arr1[] = "welcome to the group of QingYun!!!!!!!";
+	char arr2[] = "################################******";
+	int left = 0;
+	int right = strlen(arr1) - 1;//数组下标从0开始
+	while (left <= right)
+	{
+
+ 
+
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		//Sleep(600);
+		//system("cls");//清空屏幕
+		left++;
+		right--;
+	}
+
+	return 0;
+}
 //编写代码实现，模拟用户登录场景，并且只能登录三次。（只允许输入三次密码，如果密码正确则提示登录成功，如果三次均输入错误，则退出程序。）
 //int main()
 //{
@@ -559,74 +559,74 @@
 //	return 0;
 //}
 
-int idea = 0;
-int x = 0;
-
-void search()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int left = 0;
-	int right = sz - 1;
-	printf("请输入要查找的数字\n");
-	int m = 0;
-	scanf("%d", &m);
-
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		if (arr[mid] < m)
-		{
-			left = mid + 1;
-		}
-		else if (arr[mid] > m)
-		{
-			right = mid - 1;
-		}
-		else
-		{
-			printf("找到了\n");
-			break;
-		}
-	}
-
-	if (left > right)
-	{
-		printf("找不到，鬼！\n");
-	}
-
-	idea = 3;
-
-
-}
-int main()
-{
-
-	do
-	{
-		printf("找数字不？（1-20）\n");
-		scanf("%d", &idea);
-
-		switch (idea)
-		{
-		case 1:
-		{
-			while (idea == 1) {
-				search();
-			}
-			break;
-		}
-		case 0:
-		{
-			printf("不想找，鬼!\n");
-			break;
-		}
-
-		}
-		scanf("%d", &x);
-	} while (x == 0);
-	return 0;
-}
+//int idea = 0;
+//int x = 0;
+//
+//void search()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	printf("请输入要查找的数字\n");
+//	int m = 0;
+//	scanf("%d", &m);
+//
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < m)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > m)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			printf("找到了\n");
+//			break;
+//		}
+//	}
+//
+//	if (left > right)
+//	{
+//		printf("找不到，鬼！\n");
+//	}
+//
+//	idea = 3;
+//
+//
+//}
+//int main()
+//{
+//
+//	do
+//	{
+//		printf("找数字不？（1-20）\n");
+//		scanf("%d", &idea);
+//
+//		switch (idea)
+//		{
+//		case 1:
+//		{
+//			while (idea == 1) {
+//				search();
+//			}
+//			break;
+//		}
+//		case 0:
+//		{
+//			printf("不想找，鬼!\n");
+//			break;
+//		}
+//
+//		}
+//		scanf("%d", &x);
+//	} while (x == 0);
+//	return 0;
+//}
 //int func(int a)
 //{
 //	int b;
